831a5d0e669f779733dbdcad438f58a3478a33ce
domari888 <motokin128@gmail.com>
2024-03-09 19:38:46 +0900

    TypeScript でビルドできるように webpack の設定を修正

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,25 @@ const path = require('path');
 
 module.exports = {
   // 読み込むファイルを指定
-  entry: './src/index.js',
+  entry: './src/index.ts',
+
+  module: {
+    // build 時に行う追加処理
+    rules: [
+      // .ts ファイルに対して ts-loader を実行する
+      // exclude: build 対象外とするファイル
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  // モジュールとして解決するファイルの拡張子を指定
+  resolve: {
+    extensions: ['.ts'],
+  },
 
   // 出力するファイルを指定
   // dist ディレクトリに対して、ファイル名 index.js で出力
